ee7d55a09efbe70b71cafd147df839f6f001debd
saeenali <saeenali@hotmail.com>
2014-02-21 21:20:57 -0500

    Create openflow13-script.sh

---
+++ openflow13-script.sh
@@ -0,0 +1,10 @@
+ovs-vsctl set bridge s0 protocols=OpenFlow13
+ovs-vsctl set bridge s1 protocols=OpenFlow13
+ovs-vsctl set bridge s2 protocols=OpenFlow13
+ovs-vsctl set bridge s3 protocols=OpenFlow13
+ovs-vsctl set bridge s4 protocols=OpenFlow13
+ovs-vsctl set bridge s5 protocols=OpenFlow13
+ovs-vsctl set bridge s6 protocols=OpenFlow13
+ovs-vsctl set bridge s7 protocols=OpenFlow13
+ovs-vsctl set bridge s8 protocols=OpenFlow13
+ovs-vsctl set bridge s9 protocols=OpenFlow13
